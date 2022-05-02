693468a70516620def4e5ae71eee17860392b31c
김아현 <dhflxhdxhd@gmail.com>
2022-05-02 23:22:18 +0900

    불필요한 프로젝트 초기 코드 삭제

--- src/site/Main.js
+++ src/site/Main.js
@@ -10,13 +10,13 @@ function Main(props) {
         <button className="header_item"><Link to="/Mypage">mypage</Link></button> 
         </nav>
         <section className="main">
-            <div className="maintext">
-                <span className="sidetext">n개의 테마</span> 
-                <span className="sidetext2">당신의 주변 사람들이 장소를 공유합니다.</span>              
-                <div className="search">
-                    <input type="text" className="searchbox" placeholder="테마를 검색하세요!"></input>
-                </div>
-            </div>
+          <div className="maintext">
+              <span className="sidetext">n개의 테마</span> 
+              <span className="sidetext2">당신의 주변 사람들이 장소를 공유합니다.</span>              
+              <div className="search">
+                  <input type="text" className="searchbox" placeholder="테마를 검색하세요!"></input>
+              </div>
+          </div>
           <img src='/img/mainpic.png' className="mainpic"/>
           <div className="seeall">
                 <button className="all"><Link to="/alltheme"> {'>'} 전체 테마 보러가기</Link></button>     
