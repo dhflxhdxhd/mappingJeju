b84a903eaa66a0abb59999b1060556e16ceb736d
김아현 <dhflxhdxhd@gmail.com>
2022-05-14 15:15:58 +0900

    spinner 미완성

---
+++ src/site/spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import { Oval } from "react-loader-spinner";
+
+function Spinner(){
+    return(
+        <div>
+            {/* <Oval
+                ariaLabel="loading-indicator"
+                height={100}
+                width={100}
+                strokeWidth={5}
+                strokeWidthSecondary={1}
+                color="green"
+                secondaryColor="white"
+            /> */}
+            <h1>로딩 중</h1>
+            <h1>곧 spinner 넣을거임.</h1>
+        </div>
+    )
+}
+
+export default Spinner;
