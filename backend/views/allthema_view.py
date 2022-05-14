2da68d089096baa44dd5d1b6c1a5af69618e257b
김아현 <dhflxhdxhd@gmail.com>
2022-05-14 14:55:13 +0900

    백엔드 삭제 & 파일 정리

--- backend/views/allthema_view.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask import Blueprint, session, request, redirect, url_for, jsonify
-from backend import db
-
-bp = Blueprint('thema', __name__, url_prefix='/thema')
-
