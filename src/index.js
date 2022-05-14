b832199f1eb9dcec51ca10ffad6456ae7c279d15
김아현 <dhflxhdxhd@gmail.com>
2022-05-14 14:59:57 +0900

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './site/App';
 import Searchtheme from './site/Searchtheme';
-import reportWebVitals from './reportWebVitals';
 import Main from './site/Main';
 import Mypage from './site/Mypage';
 import Login from './site/Login';
@@ -17,7 +16,3 @@ root.render(
   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
