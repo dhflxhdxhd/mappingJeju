7bf53f1852bdf69256ff4bb920feaa22aeaa7bb9
lottt105 <hyobin0601@naver.com>
2022-05-17 14:53:51 +0900

    장소 등록 폼 이미지 추가

--- src/site/Createtheme.js
+++ src/site/Createtheme.js
@@ -30,7 +30,7 @@ const Createtheme = () => {
 
         <form className="searchplace" onSubmit={handleSubmit}>
           <input className="placesearchbar" placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
-          <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 22 22" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
+          <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -3 20 25" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
         </form> 
         <Map searchPlace={Place} />
 
--- src/site/Placemodal.js
+++ src/site/Placemodal.js
@@ -3,6 +3,30 @@ import '../New.css';
 
 const Placemodal = (props) => {
     const {lat, lng, place_name} = props
+    const [showImages, setShowImages] = useState([]);
+
+    // 이미지 상대경로 저장
+    const handleAddImages = (event) => {
+        const imageLists = event.target.files;
+        let imageUrlLists = [...showImages];
+
+        for (let i = 0; i < imageLists.length; i++) {
+            const currentImageUrl = URL.createObjectURL(imageLists[i]);
+            imageUrlLists.push(currentImageUrl);
+        }
+
+        if (imageUrlLists.length > 10) {
+            imageUrlLists = imageUrlLists.slice(0, 10);
+        }
+
+        setShowImages(imageUrlLists);
+        console.log(showImages)
+    };
+
+    // X버튼 클릭 시 이미지 삭제
+    const handleDeleteImage = (id) => {
+        setShowImages(showImages.filter((_, index) => index !== id));
+    };
     return(     
         <>
             <div className="modaldata">
@@ -12,13 +36,25 @@ const Placemodal = (props) => {
                 </div>
                 <div>
                     <span className="picregist">사진 등록</span>
-                    <div className="pic"></div>
-                    <div className="picplusdiv">
-                        <button className="picplus"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="plus-lg" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" /></svg></button>
+                    <label className="picpluslabel" htmlFor="image-file" onChange={handleAddImages}>
+                        <input type="file" id="image-file" multiple className="picinput" />
+                        <span className="picplus"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="plus-lg" viewBox="-7 -7 20 20">
+                        <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" /></svg></span>
+                    </label>
+                    
+                    <div className="picwrapper">
+                        {// 저장해둔 이미지들을 순회하면서 화면에 이미지 출력
+                        showImages.map((image, id) => (
+                            <span className="piccontainer" key={id}>
+                                <img className="pic" src={image} alt={`${image}-${id}`} />
+                                <button className="picdelete" onClick={() => handleDeleteImage(id)}><svg xmlns="http://www.w3.org/2000/svg" className="delete-lg" viewBox="0 0 50 50" width="20px" height="20px">
+                                <path fill="#F44336" d="M21.5 4.5H26.501V43.5H21.5z" transform="rotate(45.001 24 24)"/><path fill="#F44336" d="M21.5 4.5H26.5V43.501H21.5z" transform="rotate(135.008 24 24)"/></svg></button>
+                            </span>
+                        ))}   
                     </div>
+                                     
                 </div>
-                <div>
+                <div className="placeexplain">
                     <span className="placeexplain">장소 설명 ex)꿀팁, 가는 길...</span>
                     <input type="text" className="placeexplainbox"></input>
                 </div>
