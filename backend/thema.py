54a6d85e64095eb2c1595dfd74577a7d4077f29c
lottt105 <hyobin0601@naver.com>
2022-05-17 15:57:26 +0900

    장소 api

--- backend/thema.py
+++ backend/thema.py
@@ -72,4 +72,32 @@ def find_my_thema():
         return redirect(url_for('home'))
 
 
+# 장소 생성
+@bp.route('/', methods=['POST'])
+def add_thema_place():
+    err = ''
+    result = 'OK'
 
+    if 'user_id' in session:
+        place_data = {
+                "place_name": request.form['place_name'],
+                "lat": request.form['latitude'],
+                "lng": request.form['longitude'],
+                "address_name": request.form['address_name'],
+                "explain": request.form['explain']
+            }
+
+        try:
+            thema_id = ObjectId(request.form['thema_id'])
+            if database.thema.find_one({'_id': thema_id}) is None:
+                err = '테마가 존재하지 않습니다.'
+            else:
+                database.thema.update_one({'_id': ObjectId(request.form['thema_id'])},
+                                           {'$addToSet':
+                                                {'place': place_data}
+                                            })
+        except bson.errors.InvalidId:
+            err = '에러'
+    else:
+        err = '로그인이 필요합니다.'
+        result = ''
