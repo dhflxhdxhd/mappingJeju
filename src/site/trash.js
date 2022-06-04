3c57fb274ae6af8a666212442e30979ea9cbaf1e
김아현 <dhflxhdxhd@gmail.com>
2022-06-04 22:56:31 +0900

    삭제할 thema list 생성코드 일단 보관

---
+++ src/site/trash.js
@@ -0,0 +1,14 @@
+// const data_num = 9
+// const viewmythemeEl = document.querySelector(".viewmytheme")
+
+// console.log("themaList",MythemaList[0]["thema_name"])
+// let str = ""
+
+// for (let i= 0 ; i<9; i++){
+//   str = str +  '<div className="theme">' +
+//   '<div className="themaName">' + MythemaList[i].thema_name + '</div>' + 
+//   '<div className="themaExplain">' + MythemaList[i].thema_explain + '</div> </div>' ;
+// }
+
+// console.log(str)
+// viewmythemeEl.innerHTML = str ;
